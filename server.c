46693a932b82934acce86d6d4f748ca9aa031b94
Kai Hoffman <kbaihoff13@gmail.com>
2021-02-06 21:03:42 -0600

    Refactor server setup

--- server.c
+++ server.c
@@ -7,12 +7,13 @@
  * DATE: February 06, 2021
  */
 
-#pragma comment(lib,"Ws2_32.lib")
+#pragma comment(lib, "Ws2_32.lib")
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <WinSock2.h>
+#include "ChatKey.h"
 #include "server.h"
 
 /**
@@ -24,35 +25,16 @@
  */
 int main(int argc, char **argv)
 {
-	int port;
-
-	if (argc > 2)
-	{
-		fprintf(stderr, "%s", USAGE);
-		exit(EXIT_FAILURE);
-	}
-
-	// get the port number
-	if (argc == 2)
-	{
-		port = atoi(argv[1]);
-	}
-	else
-	{
-		port = 8080;
-	}
-
-	run_server(port);
+	run_server();
 	exit(EXIT_SUCCESS);
 }
 
-#pragma region Helpers
+#pragma region Initialization
 /**
  * @name run_server
  * @brief Run the server for clients to connect to
- * @param port The port number to connect to
  */
-void run_server(int port)
+void run_server()
 {
 	WSADATA wsa_data;
 	int connect_fd;
@@ -63,7 +45,7 @@ void run_server(int port)
 	WSAStartup(MAKEWORD(2, 2), &wsa_data);
 
 	// open a server socket for clients to connect to
-	int master_socket = open_server_socket(port);
+	int master_socket = open_server_socket(CHATKEY_PORT);
 	printf("Server listening for connections...");
 
 	// accept incoming connections to this server
@@ -71,16 +53,18 @@ void run_server(int port)
 	{
 		if ((connect_fd = accept(master_socket, (struct sockaddr *)&client_ip_addr, &addrlen)) < 0)
 		{
-			fprintf(stderr, "accept failed [%d]", WSAGetLastError());
+			fprintf(stderr, "accept failed with WSA error %d", WSAGetLastError());
 			exit(EXIT_FAILURE);
 		}
 
 		// TODO: process requests
+		printf("Server accepted client: %d", connect_fd);
 	}
 
 	// socket clean up
-	if (closesocket(master_socket) == SOCKET_ERROR) {
-		fprintf(stderr, "closesocket failed [%d]", WSAGetLastError());
+	if (closesocket(master_socket) == SOCKET_ERROR)
+	{
+		fprintf(stderr, "closesocket failed with WSA error %d", WSAGetLastError());
 		WSACleanup();
 		exit(EXIT_FAILURE);
 	}
@@ -90,10 +74,9 @@ void run_server(int port)
 /**
  * @name open_server_socket
  * @brief Create and bind the TCP server socket, then put it into the listenting state
- * @param port The port number to connect to
  * @returns The server socket file descriptor
  */
-int open_server_socket(int port)
+int open_server_socket()
 {
 	int socket_fd;
 	int opt_val = 1;
@@ -102,7 +85,7 @@ int open_server_socket(int port)
 	// socket creation for IPv4 protocol, TCP, IP
 	if ((socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET)
 	{
-		fprintf(stderr, "socket failed [%d]", WSAGetLastError());
+		fprintf(stderr, "socket failed with WSA error %d", WSAGetLastError());
 		exit(EXIT_FAILURE);
 	}
 
@@ -110,30 +93,30 @@ int open_server_socket(int port)
 	// else have to wait ~2 minutes before reusing the same port
 	if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, (char *)&opt_val, sizeof(opt_val)))
 	{
-		fprintf(stderr, "setsockopt failed [%d]", WSAGetLastError());
+		fprintf(stderr, "setsockopt failed with WSA error %d", WSAGetLastError());
 		exit(EXIT_FAILURE);
 	}
 
 	// set the IP address and port number for this server
 	server_ip_addr.sin_family = AF_INET;
 	server_ip_addr.sin_addr.S_un.S_addr = INADDR_ANY;
-	server_ip_addr.sin_port = htons(port);
+	server_ip_addr.sin_port = htons(CHATKEY_PORT);
 
 	// attach socket to the port
 	if (bind(socket_fd, (struct sockaddr *)&server_ip_addr, sizeof(server_ip_addr)))
 	{
-		fprintf(stderr, "bind failed [%d]", WSAGetLastError());
+		fprintf(stderr, "bind failed with WSA error %d", WSAGetLastError());
 		exit(EXIT_FAILURE);
 	}
 
 	// Put server in the listening state and set queue length
 	if (listen(socket_fd, QUEUE_LENGTH) < 0)
 	{
-		fprintf(stderr, "listen failed [%d]", WSAGetLastError());
+		fprintf(stderr, "listen failed with WSA error %d", WSAGetLastError());
 		exit(EXIT_FAILURE);
 	}
 
 	// success - return the successful file descriptor
 	return socket_fd;
 }
-#pragma endregion Helpers
+#pragma endregion Initialization
--- server.h
+++ server.h
@@ -2,9 +2,8 @@
 #define CHATKEY_SERVER
 
 #define QUEUE_LENGTH 5
-#define USAGE "Usage: .\\ChatKeyServer.exe <port>"
 
-void run_server(int port);
-int open_server_socket(int port);
+void run_server();
+int open_server_socket();
 
 #endif
