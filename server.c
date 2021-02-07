7e039036b921646a5e6ac25f6a6ad8863d555550
Kai Hoffman <kbaihoff13@gmail.com>
2021-02-07 04:24:39 -0600

    Implement threads to handle server messaging (bug with client_socket)

--- server.c
+++ server.c
@@ -13,9 +13,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <WinSock2.h>
+#include <Windows.h>
 #include "ChatKey.h"
 #include "server.h"
+
+/**
+ * Global server struct for this file
+ */
+struct chatkey_server cks;
 
 /**
  * @name main
@@ -38,46 +45,48 @@ int main(int argc, char **argv)
 void run_server()
 {
 	WSADATA wsa_data;
-	int server_socket, connect_fd, pid;
+	int server_socket, connect_fd, client_idx;
 	struct sockaddr_in client_ip_addr;
 	int addrlen = sizeof(client_ip_addr);
 
 	// initialize Winsock (need this for socket functions to work)
 	WSAStartup(MAKEWORD(2, 2), &wsa_data);
 
-	// open a server socket for clients to connect to
+	// open and initialize a server socket for clients to connect to
 	server_socket = open_server_socket(CHATKEY_PORT);
+	cks.num_clients = 0;
+	cks.last_activity = clock();
 	printf("Server listening for connections...\n");
 
 	// accept incoming connections to this server
-	while (1)
+	while (difftime(clock(), cks.last_activity) < INACTIVE_TIMEOUT)
 	{
-		if ((connect_fd = accept(server_socket, (struct sockaddr *)&client_ip_addr, &addrlen)) < 0)
+		if (cks.num_clients < MAX_CLIENTS)
 		{
-			fprintf(stderr, "accept failed with WSA error %d", WSAGetLastError());
-			exit(EXIT_FAILURE);
-		}
+			if ((connect_fd = accept(server_socket, (struct sockaddr *)&client_ip_addr, &addrlen)) < 0)
+			{
+				fprintf(stderr, "accept failed with WSA error %d\n", WSAGetLastError());
+				exit(EXIT_FAILURE);
+			}
 
-		// fork child process to handle messaging to/from this client
-		printf("Server accepted client: %d\n", connect_fd);
-		//pid = fork();
-		//if (pid == CLIENT_PROCESS)
-		//{
-		//closesocket(server_socket);
-		handle_communication_to_client(connect_fd);
-		printf("Client %d is disconnecting...\n", connect_fd);
-		//exit(0);
-		//}
-		//else
-		//{
-		// closesocket(connect_fd);
-		//}
+			// create thread to handle messaging to/from this client
+			add_client(connect_fd);
+
+			// new activity - update the timestamp
+			cks.last_activity = clock();
+		}
 	}
 
-	// socket clean up
+	// client clean up
+	for (client_idx = 0; client_idx < cks.num_clients; client_idx++) {
+		cleanup_communication_thread(cks.client_threads[client_idx]);
+		closesocket(cks.client_sockets[client_idx]);
+	}
+
+	// server clean up
 	if (closesocket(server_socket) == SOCKET_ERROR)
 	{
-		fprintf(stderr, "closesocket failed with WSA error %d", WSAGetLastError());
+		fprintf(stderr, "closesocket failed with WSA error %d\n", WSAGetLastError());
 		WSACleanup();
 		exit(EXIT_FAILURE);
 	}
@@ -98,7 +107,7 @@ int open_server_socket()
 	// socket creation for IPv4 protocol, TCP, IP
 	if ((socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET)
 	{
-		fprintf(stderr, "socket failed with WSA error %d", WSAGetLastError());
+		fprintf(stderr, "socket failed with WSA error %d\n", WSAGetLastError());
 		exit(EXIT_FAILURE);
 	}
 
@@ -106,7 +115,7 @@ int open_server_socket()
 	// else have to wait ~2 minutes before reusing the same port
 	if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, (char *)&opt_val, sizeof(opt_val)))
 	{
-		fprintf(stderr, "setsockopt failed with WSA error %d", WSAGetLastError());
+		fprintf(stderr, "setsockopt failed with WSA error %d\n", WSAGetLastError());
 		exit(EXIT_FAILURE);
 	}
 
@@ -119,21 +128,106 @@ int open_server_socket()
 	// attach socket to the port
 	if (bind(socket_fd, (struct sockaddr *)&server_ip_addr, sizeof(server_ip_addr)))
 	{
-		fprintf(stderr, "bind failed with WSA error %d", WSAGetLastError());
+		fprintf(stderr, "bind failed with WSA error %d\n", WSAGetLastError());
 		exit(EXIT_FAILURE);
 	}
 
 	// Put server in the listening state and set queue length
 	if (listen(socket_fd, QUEUE_LENGTH) < 0)
 	{
-		fprintf(stderr, "listen failed with WSA error %d", WSAGetLastError());
+		fprintf(stderr, "listen failed with WSA error %d\n", WSAGetLastError());
 		exit(EXIT_FAILURE);
 	}
 
 	// success - return the successful file descriptor
 	return socket_fd;
 }
+
+/**
+ * @name create_communication_thread
+ * @brief Create a thread to listen for and broadcast messages from the clients
+ * @param client_socket The socket the client is connected to
+ * @returns The thread handle
+ */
+HANDLE create_communication_thread(int client_socket)
+{
+	HANDLE thread_handle;
+	memset(&thread_handle, 0, sizeof(thread_handle));
+
+	// Default security attributes
+	// Default stack size
+	// Thread should run run_communication_thread
+	// Pass client_socket as parameter to run_communication_thread
+	// Default creation flags
+	// Don't save the thread identifier
+	thread_handle = CreateThread(NULL, 0, run_communication_thread, &client_socket, 0, NULL);
+	if (thread_handle == NULL)
+	{
+		fprintf(stderr, "CreateThread failed with WSA error %d\n", WSAGetLastError());
+	}
+
+	// success - return the successful thread handle
+	return thread_handle;
+}
+
+/**
+ * @name run_communication_thread
+ * @brief Wrapper function to handle_communication_to_client; threads should call this function
+ * @param lp_client_socket The the LP parameter of the socket this client is connected to
+ */
+DWORD WINAPI run_communication_thread(LPVOID lp_client_socket)
+{
+	int client_socket = *(int *)(lp_client_socket);
+	handle_communication_to_client(client_socket);
+	printf("Client %d is disconnecting...\n", client_socket);
+	return EXIT_SUCCESS;
+}
 #pragma endregion Initialization
+
+#pragma region ServerManagement
+/**
+ * @name add_client
+ * @brief Add a new client (socket and thread) to this server - calls create_communication_thread
+ * @param client_socket The client to add
+ */
+void add_client(int client_socket)
+{
+	printf("Server accepted client: %d\n", client_socket);
+	broadcast_message("A new user has joined the group!\n");
+	cks.client_sockets[cks.num_clients] = client_socket;
+	cks.client_threads[cks.num_clients] = create_communication_thread(client_socket);
+	cks.num_clients++;
+}
+
+/**
+ * @name remove_client
+ * @brief Remove new client from this server
+ * @param client_socket The client to remove
+ */
+void remove_client(int client_socket)
+{
+	int client_idx, shift_idx;
+
+	// find client index
+	for (client_idx = 0; client_idx < cks.num_clients; client_idx++)
+	{
+		if (cks.client_sockets[client_idx] == client_socket)
+		{
+			break;
+		}
+	}
+
+	// cleanup thread
+	cleanup_communication_thread(cks.client_threads[client_idx]);
+
+	// shift client sockets and threads
+	for (shift_idx = client_idx + 1; shift_idx < cks.num_clients; shift_idx++) {
+		cks.client_sockets[shift_idx - 1] = cks.client_sockets[shift_idx];
+		cks.client_threads[shift_idx - 1] = cks.client_threads[shift_idx];
+	}
+	cks.num_clients--;
+}
+#pragma endregion ServerManagement
 
 #pragma region Messaging
 /**
@@ -142,7 +236,7 @@ int open_server_socket()
  * @param cks The ChatKey server that should broadcast the messages
  * @param msg The message to broadcast
  */
-void broadcast_message(struct chatkey_server cks, char *msg)
+void broadcast_message(char *msg)
 {
 	int i;
 	for (i = 0; i < cks.num_clients; i++)
@@ -159,11 +253,11 @@ void broadcast_message(struct chatkey_server cks, char *msg)
  */
 void send_message(int client_socket, char *buffer)
 {
-  if (send(client_socket, buffer, sizeof(buffer), 0) == SOCKET_ERROR)
-  {
-    fprintf(stderr, "send failed with WSA error %d", WSAGetLastError());
-    return;
-  }
+	if (send(client_socket, buffer, sizeof(buffer), 0) == SOCKET_ERROR)
+	{
+		fprintf(stderr, "send failed with WSA error %d\n", WSAGetLastError());
+		return;
+	}
 }
 
 /**
@@ -183,7 +277,8 @@ void handle_communication_to_client(int client_fd)
 		bytes_read = recv(client_fd, buffer, sizeof(buffer), 0);
 		if (bytes_read == SOCKET_ERROR)
 		{
-			fprintf(stderr, "recv failed with WSA error %d", WSAGetLastError());
+			fprintf(stderr, "recv failed with WSA error %d\n", WSAGetLastError());
+			break;
 		}
 		else if (bytes_read > 0)
 		{
@@ -193,6 +288,7 @@ void handle_communication_to_client(int client_fd)
 		if (stop_communication(buffer))
 		{
 			send_message(client_fd, DISCONNECT_CLIENT_MSG);
+			remove_client(client_fd);
 			break;
 		}
 	}
@@ -206,19 +302,30 @@ void handle_communication_to_client(int client_fd)
  */
 int stop_communication(char *buffer)
 {
-  if (strncmp(buffer, EXIT_MSG, strlen(EXIT_MSG)) == 0)
-  {
-    return 1;
-  }
-  if (strncmp(buffer, QUIT_MSG, strlen(QUIT_MSG)) == 0)
-  {
-    return 1;
-  }
-  if (strncmp(buffer, DISCONNECT_CLIENT_MSG, strlen(DISCONNECT_CLIENT_MSG)) == 0)
-  {
-    return 1;
-  }
-  return 0;
+	if (strncmp(buffer, EXIT_MSG, strlen(EXIT_MSG)) == 0)
+	{
+		return 1;
+	}
+	if (strncmp(buffer, QUIT_MSG, strlen(QUIT_MSG)) == 0)
+	{
+		return 1;
+	}
+	if (strncmp(buffer, DISCONNECT_CLIENT_MSG, strlen(DISCONNECT_CLIENT_MSG)) == 0)
+	{
+		return 1;
+	}
+	return 0;
 }
-
 #pragma endregion Messaging
+
+#pragma region Cleanup
+/**
+ * @name cleanup_communication_thread
+ * @brief Clean up thread resources and close handles
+ * @param thread_handle The thread to close
+ */
+void cleanup_communication_thread(HANDLE thread_handle)
+{
+	CloseHandle(thread_handle);
+}
+#pragma endregion Cleanup
--- server.h
+++ server.h
@@ -1,19 +1,23 @@
 #ifndef CHATKEY_SERVER
 #define CHATKEY_SERVER
 
-#define CLIENT_PROCESS 0
-#define MAX_CLIENTS 2
+#define INACTIVE_TIMEOUT 30000
+#define MAX_CLIENTS 5
 #define QUEUE_LENGTH 5
 
 struct chatkey_server
 {
   int num_clients;
   int client_sockets[MAX_CLIENTS];
+  HANDLE client_threads[MAX_CLIENTS];
+  clock_t last_activity;
 };
 
 void run_server();
 int open_server_socket();
-void broadcast_message(struct chatkey_server cks, char *msg);
-void handle_communication_to_client();
+void add_client(int client_socket);
+void remove_client(int client_socket);
+void broadcast_message(char *msg);
+void handle_communication_to_client(int client_fd);
 
 #endif
